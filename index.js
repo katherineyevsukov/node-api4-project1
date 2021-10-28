ad8900b18dac72916b55ec9809ec5cc8be6c9c13
Kat <kyevsukov@gmail.com>
2021-10-28 17:50:31 -0400

    can login

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const PORT = process.env.PORT || 5000
 app.get('/api/users', (req, res) => {
     res.json({
         users: [
-            {name: 'Sam'},
-            {name: "Kat"},
-            {name: "Clem"}
+            {username: 'Sam'},
+            {username: "Kat"},
+            {username: "Clem"}
         ]
     })
 })
@@ -20,6 +20,14 @@ app.get('/api/users', (req, res) => {
 app.post('/api/register', (req, res) => {
     const newUser = req.body
     res.status(201).json(newUser)
+})
+
+app.post('/api/login', (req, res) => {
+    if(req.body.username && req.body.password){
+        res.status(200).json({ message: `Welcome ${req.body.username}`})
+    } else {
+        res.status(400).json({message: 'you are missing username or password'})
+    }
 })
 
 app.listen(PORT, () => {
